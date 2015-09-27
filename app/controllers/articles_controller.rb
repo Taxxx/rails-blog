32eacedaaf73e2838824d187c6ee72005a7f5106
Henrry Chuquimia <henrry.el.master@gmail.com>
2015-09-26 21:44:36 -0400

    count visits finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,25 @@
 class ArticlesController < ApplicationController
+	#before_action :validate_user, except: [:show, :index]
+	before_action :authenticate_user!, except: [:show, :index] #el inverso de except es only
+	#el siguiente callback nos permite limpiar el codigo, manteniendo DRY
+	before_action :set_article, except: [:index, :new, :create]
 	#GET /articles
 	def index
 		@articles = Article.all
 	end
 	#GET /articles/:id
 	def show
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 		#Where
 		#Article.where("body LIKE ?","%hola%")
+		@article.update_visit_count
 	end
 	#GET /articles/new
 	def new
 		@article = Article.new 
 	end
 	def edit
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 	end
 	#POST /articles
 	def create
@@ -32,7 +37,7 @@ class ArticlesController < ApplicationController
 	#PUT /articles/:id
 	def update
 		#@article.update_attributes({title: "New Title"})
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 		if @article.update(article_params)
 			redirect_to @article
 		else
@@ -42,12 +47,20 @@ class ArticlesController < ApplicationController
 	#DELETE /articles/:id
 	def destroy
 		#DELETE FROM articles
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
 	end
 
 	private
+	#Aca definiendo set_article para usarlos con before_action
+	def set_article
+		@article = Article.find(params[:id])
+	end
+	#Mi validacion privada
+	def validate_user
+		redirect_to new_user_session_path, notice: "Please Login :@"
+	end
 
 	def article_params
 		params.require(:article).permit(:title,:body)
--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,17 @@ class Article < ActiveRecord::Base
 	validates :title, presence: true, uniqueness: true
 	validates :body, presence: true, length: {minimum: 10}
 	#validates :username, format: {with: /regex/}
+	before_save :set_visit_count
+
+	def update_visit_count
+		#self.save if self.visit_count.nil?
+		self.update(visit_count: self.visit_count + 1)
+	end
+
+	private
+
+	def set_visit_count
+		#self.visit_count = 0
+		self.visit_count ||= 0
+	end
 end
